22d892b7c2fa533c3d7b592d171a3360a3a43e8f
Jay Kelly <comital333@gmail.com>
2018-04-21 11:34:17 +1000

    stringify block

--- websocket.js
+++ websocket.js
@@ -1,12 +1,11 @@
 const WebSocket = require('ws')
-const wss = new WebSocket.Server({port: 8080})
+const wss = new WebSocket.Server({port: 5000})
 const Web3 = require('web3')
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         console.log('recieved: ', message)
     })
-    ws.send('something')
 })
 wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
