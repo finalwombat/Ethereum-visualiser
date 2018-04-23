734d0021e80c11aefd64a258c32cd35ed5506158
Jay Kelly <comital333@gmail.com>
2018-04-23 21:21:05 +1000

    recieve all block data

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 
 
 const Block =  (props) => {
+  const block = props.block
     return (
       <div className="block">
-        <h4>{props.number}</h4>
+        <h4># {block.number}</h4>
+        <h4>Transaction: {block.transactions.length}</h4>
       </div>
     );
 }
