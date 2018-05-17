69c741351638e5e6cbd6eabf10fa2dc55731209a
Jay Kelly <comital333@gmail.com>
2018-05-17 11:13:59 +1000

    change name to suit new api

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 
 
 const Block =  (props) => {
+  console.log("props:", props)
   const block = props.block
     return (
       <div className="block">
-        <h4># {block.number}</h4>
+        <h4># {block.blockdata.number}</h4>
         <summary>Transaction: {block.transactions.length}</summary>
         <details>
           <ul>{block.transactions.map((transaction) => {
-            return <li>{transaction}</li>
+            return <li>{transaction.blockHash}</li>
           })}</ul>
         </details>
       </div>
