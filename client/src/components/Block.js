fe3487a23dd96ffbaea9278332e7f65401b417b1
Jay Kelly <comital333@gmail.com>
2018-05-23 21:20:38 +1000

    moved styling from blocks to block

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,19 +1,47 @@
-import React, { Component } from 'react';
-import TransactionList from './TransactionList'
-import styled from 'styled-components'
+import React, { Component } from "react";
+import TransactionList from "./TransactionList";
+import styled from "styled-components";
 
-const Block =  (props) => {
-  console.log("props:", props)
-  const block = props.block
-    return (
-      <div className="block">
-        <h4># {block.blockdata.number}</h4>
-        <summary>Transaction: {block.transactions.length}</summary>
-        <details>
-          <TransactionList transactions={block.transactions} />>
-        </details>
-      </div>
-    );
+const Block = props => {
+  console.log("props:", props);
+  const block = props.block;
+  const color = getColor(block.transactions.length);
+  return (
+    <BlockDiv className="block" background={color}>
+      <h4># {block.blockdata.number}</h4>
+      <summary>Transaction: {block.transactions.length}</summary>
+      <details>
+        <TransactionList transactions={block.transactions} />>
+      </details>
+    </BlockDiv>
+  );
+};
+
+export default Block;
+
+// component style
+const BlockDiv = styled.div`
+  background: ${props => props.background};
+  width: 100%;
+  border-radius: 20px;
+`;
+
+// helper functions
+
+function getColor(number) {
+  const colors = [
+    "#808080",
+    "#ff00ff",
+    "#7f00ff",
+    "#0080ff",
+    "#00ff80",
+    "#00ff00",
+    "#80ff00",
+    "#ffff00",
+    "#ff8000",
+    "#ff0000"
+  ];
+  number = number / 10;
+  if (number > 9) number = 9;
+  return colors[Math.floor(number)];
 }
-
-export default Block
--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -1,52 +1,50 @@
-import React, { Component } from 'react';
-import Block from './Block'
-import './Blocks.css'
-import posed, { PoseGroup } from 'react-pose'
-import styled from 'styled-components'
+import React, { Component } from "react";
+import Block from "./Block";
+import "./Blocks.css";
+import posed, { PoseGroup } from "react-pose";
+import styled from "styled-components";
 
 class Blocks extends Component {
   constructor(props) {
-      super(props)
-      this.state = {
-          blocks: []
-      }
+    super(props);
+    this.state = {
+      blocks: []
+    };
   }
 
   componentDidMount() {
-    const ws = new WebSocket('ws://localhost:5000')
-    
+    const ws = new WebSocket("ws://localhost:5000");
+
     ws.onopen = () => {
-        console.log('websocket is connected...')
-        ws.send('connected')
-    }
-    ws.onmessage = (ev) => {
-      const data = JSON.parse(ev.data)
+      console.log("websocket is connected...");
+      ws.send("connected");
+    };
+    ws.onmessage = ev => {
+      const data = JSON.parse(ev.data);
       this.setState(prevState => ({
         blocks: [...prevState.blocks, data]
-      }))
-    }
-    
+      }));
+    };
   }
- 
+
   render() {
-    const blocksList = this.state.blocks.map((block) => { 
-      const color = getColor(block.transactions.length)
-      console.log(color)
-      return <BlockDiv background={color}><Block block={block} /></BlockDiv>
-    })
+    const blocksList = this.state.blocks.map(block => {
+      return (
+        <Item>
+          <Block block={block} />
+        </Item>
+      );
+    });
     return (
       <BlocksDiv className="blocks">
         <h2>New Blocks</h2>
-        <PoseGroup className="blocksGroup">
-          {blocksList}
-        </PoseGroup>
-
+        <PoseGroup className="blocksGroup">{blocksList}</PoseGroup>
       </BlocksDiv>
     );
   }
 }
 
-export default Blocks
+export default Blocks;
 
 // component style
 
@@ -62,36 +60,8 @@ const BlocksDiv = styled.div`
     color: white;
     background: black;
   }
-`
+`;
 const Item = posed.div({
-  enter: { opacity: 1},
+  enter: { opacity: 1 },
   exit: { opacity: 0 }
-})
-
-const BlockDiv = styled(Item)`
-  background: ${props => props.background};
-  width: 100%;
-  border-radius: 20px;
-`
-
-// helper functions
-
-function getColor(number) {
-
-  const colors = 
-  [
-    "#808080",
-    "#ff00ff",
-    "#7f00ff",
-    "#0080ff",
-    "#00ff80",
-    "#00ff00",
-    "#80ff00",
-    "#ffff00",
-    "#ff8000",
-    "#ff0000"
-  ]
-  number = number / 10
-  if(number > 9 ) number = 9;
-  return colors[Math.floor(number)]
-}
+});
