c259a4c293e9e3dd3f9a73c6fc3a7eed3a720634
Jay Kelly <comital333@gmail.com>
2018-05-30 12:35:27 +1000

    now a functional component

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Blocks from './components/Blocks'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      blocks: []
-    }
 
-  }
-  render() {
-    return (
-      <div className="App">
+const App = () => {
+  return (
+    <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to Blockchain Visualiser</h1>
         </header>
         <main>
-          <Blocks blocks={this.state.blocks} />
+          <Blocks blocks />
         </main>
-        
-        
       </div>
-    );
-  }
+  )
+  
 }
 
 export default App;
