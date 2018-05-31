d45177e0857180cdc5dd7edd7b493e917fee65d2
Jay Kelly <comital333@gmail.com>
2018-05-31 23:23:18 +1000

    added blocks reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
-// import BlocksReducer from './reducer_blocks'
+import blocks from './reducer_blocks'
 import selection from './reducer_currentSelection'
 
 export default combineReducers({
+    blocks: blocks,
     currentSelection: selection
 })
--- client/src/reducers/reducer_blocks.js
+++ client/src/reducers/reducer_blocks.js
@@ -1,38 +1,36 @@
 import { combineReducers } from 'redux'
 
-const block = (state, action) => {
-    switch (action.type) {
-        case 'ADD_BLOCK':
-            return {
-                id: action.id,
-                data: action.block
-            }
-        default:
-            return state
-    }
-}
+// const block = (state, action) => {
+//     switch (action.type) {
+//         case 'ADD_BLOCK':
+//             return {
+//                 id: action.id,
+//                 data: action.block
+//             }
+//         default:
+//             return state
+//     }
+// }
 
 const blocks = (state = [], action) => {
     switch (action.type) {
         case 'ADD_BLOCK': 
             return [
                 ...state,
-                block(undefined, action)
+                action.payload
             ]
-        case 'SET_CURRENT_BLOCK': 
-            return action.id
         default: 
             return state
     }
 }
 
 
-const BlocksReducer = combineReducers(block, blocks)
-export default BlocksReducer
+// const BlocksReducer = combineReducers(block, blocks)
+export default blocks
 
-state = {
-    blocks: [],
-    transactions: [],
-    currentBlocK: 'id',
-    currentTransaction: 'id'
-}
+// state = {
+//     blocks: [],
+//     transactions: [],
+//     currentBlocK: 'id',
+//     currentTransaction: 'id'
+// }
