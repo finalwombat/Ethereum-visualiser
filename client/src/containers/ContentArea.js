caae4c44cb5f6f53851bb2a866ed4a2d4b0c2a8f
Jay Kelly <comital333@gmail.com>
2018-05-31 12:30:03 +1000

    inti content area and add redux bindings

--- client/src/containers/ContentArea.js
+++ client/src/containers/ContentArea.js
@@ -1,3 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import 
+
+const ContentArea = ({block}) => {
+    console.log("block: ", block)
+    return (
+        <div>Selected block: {block.blockdata ? block.blockdata.number : 'null'}</div>
+    )
+}
+
+function mapStateToProps(state) {
+    return {
+        block: state.currentSelection
+    }
+}
+
+export default connect(mapStateToProps)(ContentArea)
