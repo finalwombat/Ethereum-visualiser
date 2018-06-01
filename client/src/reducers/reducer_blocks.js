a758fe57c32f09c64decce861cdf1d851a6515c2
Jay Kelly <comital333@gmail.com>
2018-06-01 12:43:00 +1000

    removed commented out code and added comment

--- client/src/reducers/reducer_blocks.js
+++ client/src/reducers/reducer_blocks.js
@@ -1,36 +1,11 @@
-import { combineReducers } from 'redux'
-
-// const block = (state, action) => {
-//     switch (action.type) {
-//         case 'ADD_BLOCK':
-//             return {
-//                 id: action.id,
-//                 data: action.block
-//             }
-//         default:
-//             return state
-//     }
-// }
-
+// Add a new block to the blocks array - only called by websocket.js
 const blocks = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_BLOCK': 
-            return [
-                ...state,
-                action.payload
-            ]
-        default: 
-            return state
-    }
-}
+  switch (action.type) {
+    case "ADD_BLOCK":
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
 
-
-// const BlocksReducer = combineReducers(block, blocks)
-export default blocks
-
-// state = {
-//     blocks: [],
-//     transactions: [],
-//     currentBlocK: 'id',
-//     currentTransaction: 'id'
-// }
+export default blocks;
