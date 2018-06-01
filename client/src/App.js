d5ef6eac041f6953dfd2bf9aaa0da598f1cd444f
Jay Kelly <comital333@gmail.com>
2018-06-01 12:37:11 +1000

    prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import './App.css';
-import Blocks from './components/Blocks'
-import ContentArea from './containers/ContentArea'
-
+import React from "react";
+import "./App.css";
+import Blocks from "./components/Blocks";
+import ContentArea from "./containers/ContentArea";
 
 const App = () => {
   return (
     <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Blockchain Visualiser</h1>
-        </header>
-        <main>
-          <Blocks />
-          <ContentArea />
-        </main>
-      </div>
-  )
-  
-}
+      <header className="App-header">
+        <h1 className="App-title">Welcome to Blockchain Visualiser</h1>
+      </header>
+      <main>
+        <Blocks />
+        <ContentArea />
+      </main>
+    </div>
+  );
+};
 
 export default App;
