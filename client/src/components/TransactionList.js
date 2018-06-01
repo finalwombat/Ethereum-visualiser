b036b039aece4bd79063d1936b44837df384543e
Jay Kelly <comital333@gmail.com>
2018-06-01 12:58:08 +1000

    comment and prettier

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react';
-import Transaction from './Transaction'
+import React, { Component } from "react";
+import Transaction from "./Transaction";
 
-const TransactionsList =  (props) => {
+// List of transactions
+const TransactionsList = props => {
   const transactions = props.transactions.map(transaction => {
-    return <Transaction data={transaction} />
-  })
-    return (
-      <div className="transactions">
-        {transactions}
-      </div>
-    );
-}
+    return <Transaction data={transaction} />;
+  });
+  return <div className="transactions">{transactions}</div>;
+};
 
-export default TransactionsList
+export default TransactionsList;
