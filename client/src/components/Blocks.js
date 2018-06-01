368daf0cc762a94ad0b753a65615d1dc33ba94d4
Jay Kelly <comital333@gmail.com>
2018-06-01 12:50:57 +1000

    comment and prettier

--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -1,39 +1,41 @@
-import React, { Component } from "react"
-import Block from "./Block"
-import "./Blocks.css"
-import { connect } from 'react-redux'
-import { selectBlock } from '../actions/index'
-import { bindActionCreators } from 'redux'
-import posed, { PoseGroup } from "react-pose"
-import styled from "styled-components"
+import React, { Component } from "react";
+import Block from "./Block";
+import "./Blocks.css";
+import { connect } from "react-redux";
+import { selectBlock } from "../actions/index";
+import { bindActionCreators } from "redux";
+import posed, { PoseGroup } from "react-pose";
+import styled from "styled-components";
 
 const Blocks = ({ blocks, selectBlock }) => {
-    const blocksList = blocks.map(block => {
-      return (
-        <Item onClick={() => selectBlock(block)}>
-          <Block block={block} />
-        </Item>
-      );
-    });
+  // map blocks to animated div list
+  const blocksList = blocks.map(block => {
     return (
-      <BlocksDiv className="blocks">
-        <h2>New Blocks</h2>
-        <PoseGroup className="blocksGroup">{blocksList}</PoseGroup>
-      </BlocksDiv>
-    )
-}
+      <Item onClick={() => selectBlock(block)}>
+        <Block block={block} />
+      </Item>
+    );
+  });
+  return (
+    <BlocksDiv className="blocks">
+      <h2>New Blocks</h2>
+      <PoseGroup className="blocksGroup">{blocksList}</PoseGroup>
+    </BlocksDiv>
+  );
+};
 
-function mapStateToProps(state){
+// Connect to redux
+function mapStateToProps(state) {
   return {
     blocks: state.blocks
-  }
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ selectBlock: selectBlock}, dispatch)
+  return bindActionCreators({ selectBlock: selectBlock }, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Blocks)
+export default connect(mapStateToProps, mapDispatchToProps)(Blocks);
 
 // component style
 
