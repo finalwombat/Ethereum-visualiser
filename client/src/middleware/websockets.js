a036b8968321bc5eff4324da82e818e2f268826e
Jay Kelly <comital333@gmail.com>
2018-06-01 12:46:00 +1000

    changed var name to message + comment

--- client/src/middleware/websockets.js
+++ client/src/middleware/websockets.js
@@ -1,16 +1,16 @@
-import { bindActionCreators } from 'redux'
-import { addBlock } from '../actions/index'
+import { bindActionCreators } from "redux";
+import { addBlock } from "../actions/index";
 
-export default function listener(dispatch){
-    const ws = new WebSocket("ws://localhost:5000");
+// listens for new blocks from server then dispatches action
+export default function listener(dispatch) {
+  const ws = new WebSocket("ws://localhost:5000");
 
-    ws.onopen = () => {
-      console.log("websocket is connected...");
-      ws.send("connected");
-    };
-    ws.onmessage = ev => {
-        console.log('new block')
-      const data = JSON.parse(ev.data);
-      dispatch(addBlock(data))
-    };
+  ws.onopen = () => {
+    console.log("websocket is connected...");
+    ws.send("connected");
+  };
+  ws.onmessage = message => {
+    const data = JSON.parse(message.data);
+    dispatch(addBlock(data));
+  };
 }
