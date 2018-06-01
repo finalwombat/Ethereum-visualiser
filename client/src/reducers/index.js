3311da17af4194e6995781bbd1bd9a8b77a20c1c
Jay Kelly <comital333@gmail.com>
2018-06-01 12:43:33 +1000

    prettier

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,8 @@
-import { combineReducers } from 'redux'
-import blocks from './reducer_blocks'
-import selection from './reducer_currentSelection'
+import { combineReducers } from "redux";
+import blocks from "./reducer_blocks";
+import selection from "./reducer_currentSelection";
 
 export default combineReducers({
-    blocks: blocks,
-    currentSelection: selection
-})
+  blocks: blocks,
+  currentSelection: selection
+});
