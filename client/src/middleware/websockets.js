26f94a471daf496d9fbcd05b8be3851ce006c444
Jay Kelly <comital333@gmail.com>
2018-06-05 07:44:16 +1000

    added web3 websocket listener

--- client/src/middleware/websockets.js
+++ client/src/middleware/websockets.js
@@ -1,16 +1,68 @@
+import Web3 from "web3";
 import { bindActionCreators } from "redux";
 import { addBlock } from "../actions/index";
 
-// listens for new blocks from server then dispatches action
 export default function listener(dispatch) {
-  const ws = new WebSocket("ws://localhost:5000");
 
-  ws.onopen = () => {
-    console.log("websocket is connected...");
-    ws.send("connected");
-  };
-  ws.onmessage = message => {
-    const data = JSON.parse(message.data);
-    dispatch(addBlock(data));
-  };
+    // set the provider you want from Web3.providers
+    const web3 = new Web3(
+      new Web3.providers.WebsocketProvider("wss://rinkeby.infura.io/ws")
+    );
+    const web3Http = new Web3(
+      new Web3.providers.HttpProvider(
+        "https://rinkeby.infura.io/VYDF3dhGnawnGEaGGnAg"
+      )
+    );
+
+  //  subscribe for new block headers
+  web3.eth
+    .subscribe("newBlockHeaders", (error, result) => {
+      if (error) {
+        console.log(error);
+      }
+    })
+    .on("data", blockHeader => {
+      web3Http.eth
+        .getBlock(blockHeader.number)
+        .catch(err => {
+          console.log(err);
+        })
+        .then(block => {
+          if (block) {
+            // Check for transactions
+            if (block.transactions.length) {
+              const transactionNumbers = block.transactions;
+              // Get transaction infromation for each transaction
+              const promises = transactionNumbers.map(transactionNumber => {
+                return web3Http.eth
+                  .getTransaction(transactionNumber)
+                  .catch(err => {
+                    console.log(err);
+                  })
+                  .then(transaction => {
+                    return transaction;
+                  });
+              });
+              Promise.all(promises).then(transactions => {
+                const data = { blockdata: block, transactions };
+                dispatch(addBlock(data));
+              });
+            }
+          }
+        });
+    });
 }
+
+// listens for new blocks from server then dispatches action
+// export default function listener(dispatch) {
+//   const ws = new WebSocket("ws://localhost:5000");
+
+//   ws.onopen = () => {
+//     console.log("websocket is connected...");
+//     ws.send("connected");
+//   };
+//   ws.onmessage = message => {
+//     const data = JSON.parse(message.data);
+//     dispatch(addBlock(data));
+//   };
+// }
