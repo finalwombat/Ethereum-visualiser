2cb27d9423f826e6682b0d24464e5a80eb8f7420
Jay Kelly <comital333@gmail.com>
2018-06-05 22:45:10 +1000

    redux dev tools extension

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,10 @@ import { Provider } from "react-redux";
 import rootReducer from "./reducers";
 import listener from "./middleware/websockets";
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 // websocket connection to update blocks
 listener(store.dispatch);
 
