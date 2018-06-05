d12d2af19188024625d417ef36419f0177d14f7c
Jay Kelly <comital333@gmail.com>
2018-06-05 10:31:59 +1000

    styled component

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,37 @@ import React from "react";
 import "./App.css";
 import Blocks from "./components/Blocks";
 import ContentArea from "./containers/ContentArea";
+import styled from 'styled-components'
 
 const App = () => {
   return (
-    <div className="App">
+    <AppDiv className="App">
       <header className="App-header">
         <h1 className="App-title">Welcome to Blockchain Visualiser</h1>
       </header>
-      <main>
+      <ContentDiv>
         <Blocks />
         <ContentArea />
-      </main>
-    </div>
+      </ContentDiv>
+    </AppDiv>
   );
 };
 
 export default App;
+const AppDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+`
+const ContentDiv = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    height: 100%;
+
+    & .blocks {
+      width: 20%;
+      margin-top: 20px;
+    }
+`
