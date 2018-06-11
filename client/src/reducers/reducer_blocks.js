9d0bb41f882b84ac0433ffa581bc3e70139890cb
Jay Kelly <comital333@gmail.com>
2018-06-11 21:59:38 +1000

    added check for duplicate block

--- client/src/reducers/reducer_blocks.js
+++ client/src/reducers/reducer_blocks.js
@@ -2,10 +2,19 @@
 const blocks = (state = [], action) => {
   switch (action.type) {
     case "ADD_BLOCK":
-      return [action.payload, ...state];
+      return addBlock(state, action.payload)
     default:
       return state;
   }
 };
 
 export default blocks;
+
+function addBlock(state, payload){
+  console.log(payload.number)
+
+  // check that the block does not already exist in state
+  return state.filter(block => block.number == payload.number).length ? 
+    state :
+    [payload, ...state]
+}
