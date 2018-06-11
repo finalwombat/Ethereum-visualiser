2b0ca9fd3eb987ec6a1bc33767d52c12c293d2ad
Jay Kelly <comital333@gmail.com>
2018-06-12 09:27:56 +1000

    listen for pending transactions - update state every second with count

--- client/src/middleware/websockets.js
+++ client/src/middleware/websockets.js
@@ -1,16 +1,17 @@
 import Web3 from "web3";
 import { bindActionCreators } from "redux";
 import { addBlock } from "../actions/index";
+import { updateTransactionCount } from "../actions/index"
 
 export default function listener(dispatch) {
 
     // set the provider you want from Web3.providers
     const web3 = new Web3(
-      new Web3.providers.WebsocketProvider("wss://rinkeby.infura.io/_ws")
+      new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/_ws")
     );
     const web3Http = new Web3(
       new Web3.providers.HttpProvider(
-        "https://rinkeby.infura.io/VYDF3dhGnawnGEaGGnAg"
+        "https://mainnet.infura.io/VYDF3dhGnawnGEaGGnAg"
       )
     );
 
@@ -33,6 +34,23 @@ export default function listener(dispatch) {
           }
         });
     });
+
+    //  subscribe for new transaction headers
+  let transactions = []
+  // dispatch transaction count every second - then reset
+  setInterval(() => {
+    dispatch(updateTransactionCount(transactions.length))
+    transactions = []
+  }, 1000)
+  web3.eth
+  .subscribe("pendingTransactions", (error, result) => {
+    if (error) {
+      console.log(error);
+    }
+  })
+  .on("data", transaction => {
+    transactions.push(transaction)
+  });
 }
 
 // listens for new blocks from server then dispatches action
