fabb827b8613890ce5ed5fe7b2c67a4b37ae7452
Jay Kelly <comital333@gmail.com>
2018-06-12 21:37:44 +1000

    add network reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import blocks from "./reducer_blocks";
 import selection from "./reducer_currentSelection";
 import transactionCount from "./reducer_updateTransactionCount";
+import currentNetwork from './reducer_network'
 
 export default combineReducers({
   blocks: blocks,
   currentSelection: selection,
-  transactionCount: transactionCount
+  transactionCount: transactionCount,
+  currentNetwork: currentNetwork
 });
