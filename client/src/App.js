4a9d66fe5dcdd20bb625b834b56fa950225b2a66
Jay Kelly <comital333@gmail.com>
2018-06-12 12:04:23 +1000

    add header and remove transactions

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header"
 import Blocks from "./components/Blocks";
 import ContentArea from "./containers/ContentArea";
 import Transactions from "./containers/Transactions"
@@ -8,13 +9,10 @@ import styled from 'styled-components'
 const App = () => {
   return (
     <AppDiv className="App">
-      <header className="App-header">
-        <h1 className="App-title">Welcome to Blockchain Visualiser</h1>
-      </header>
+      <Header />
       <ContentDiv>
         <Blocks/>
         <ContentArea />
-        <Transactions />
       </ContentDiv>
     </AppDiv>
   );
