49e4e36b6f207739398ba60cb69e747124e168e4
Jay Kelly <comital333@gmail.com>
2018-06-12 12:06:30 +1000

    use transactionCount component

--- client/src/containers/Transactions.js
+++ client/src/containers/Transactions.js
@@ -2,10 +2,13 @@ import React from "react";
 import { connect } from "react-redux";
 import posed, { PoseGroup } from "react-pose";
 import styled from "styled-components";
+import TransactionCount from "../components/TransactionCount"
 
 const Transactions = ({ transactionCount}) => {
   return (
-    <div>{transactionCount}</div>
+    <TransactionsDiv>
+        <TransactionCount count={transactionCount} />
+    </TransactionsDiv>
   );
 };
 
@@ -20,33 +23,7 @@ export default connect(mapStateToProps)(Transactions);
 
 // component style
 
-const WrapperDiv = styled.div`
-  height: 100%;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  background: rgba(0, 0, 0, 0.6);
-  border: 1px solid rgba(40, 40, 40, 0.3);
-  -webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
-  -moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
-  box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
-  & h2 {
-    margin: 0;
-    margin-bottom: 15px;
-    padding: 15px;
-    height: 40px;
-    color: #E27D60;
-  }
+const TransactionsDiv = styled.div`
+    background: #282828;
+    color: red;
 `
-const BlocksDiv = styled.div`
-  height: 100%;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  padding: 10px;
-  overflow-y: scroll;
-`;
-const Item = posed.div({
-  enter: { opacity: 1 },
-  exit: { opacity: 0 }
-});
