353557f9d7b78f73804522d817388ec0e97c464c
Jay Kelly <comital333@gmail.com>
2018-06-19 10:50:26 +1000

    update to work with listener factory

--- client/src/index.js
+++ client/src/index.js
@@ -6,14 +6,26 @@ import registerServiceWorker from "./registerServiceWorker";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers";
-import listener from "./middleware/websockets";
+import Listener from "./middleware/websockets";
+import watch from 'redux-watch';
 
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+let listener = new Listener(store, "mainnet");
+listener.start();
 // websocket connection to update blocks
-listener(store.dispatch);
+const w = watch(store.getState, 'currentNetwork')
+store.subscribe(w((newVal, oldVal, objectPath) => {
+  if(newVal !== oldVal){
+    listener.stop();
+    listener = new Listener(store, newVal);
+    listener.start();
+  }
+}))
+
 
 ReactDOM.render(
   <Provider store={store}>
