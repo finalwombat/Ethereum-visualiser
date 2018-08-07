e86a22122456f9647e143488669ae395f663740e
Jay Kelly <comital333@gmail.com>
2018-08-07 12:34:32 +1000

    init component

---
+++ src/components/Chart.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts'
+
+const Chart = ({data, color}) => {
+    data = convertData(data)
+    console.log(data)
+    return(
+        <LineChart width={600} height={200} color="white" data={data}>
+            <Line type="monotone" dataKey="uv" stroke={color} />
+            <XAxis dataKey="name" stroke="white"/>
+            <YAxis stroke="white"/>
+        </LineChart>
+    )
+}
+
+export default Chart
+
+function convertData(data){
+    const result = data.map((item) => {
+        return {name: '', uv: item}
+    })
+    return result
+}
